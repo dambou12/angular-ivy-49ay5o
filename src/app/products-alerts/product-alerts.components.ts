a44e5677d15f173b665150dce53512d56f80a816
dambou12 <dambouhilaire@yahoo.com>
2021-08-08 13:44:59 -0700

    1 file modified

--- src/app/products-alerts/product-alerts.components.ts
+++ src/app/products-alerts/product-alerts.components.ts
@@ -1,4 +1,4 @@
-<-- import { Component, OnInit} from '@angular/core';
+ import { Component, OnInit} from '@angular/core';
 import {Input} from '@angular/core';
 import {Product} from '../products';
 
@@ -15,4 +15,4 @@ ngOnInit(){
 
 }
 
-} -->
+} 
