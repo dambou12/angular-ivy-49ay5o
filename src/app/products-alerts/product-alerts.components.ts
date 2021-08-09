7fc23cf3c9638557f90a81856f901c4fc9ee0a6f
dambou12 <dambouhilaire@yahoo.com>
2021-08-08 20:26:04 -0700

    1 file modified

--- src/app/products-alerts/product-alerts.components.ts
+++ src/app/products-alerts/product-alerts.components.ts
@@ -1,18 +1,14 @@
- import { Component, OnInit} from '@angular/core';
-import {Input} from '@angular/core';
-import {Product} from '../products';
+import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
+import { Product } from '../products';
 
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
-  styleUrls:['./product-alerts.component.css']
+  styleUrls: ['./product-alerts.component.css']
 })
-
-export class ProductAlertsComponent implements OnInit{
+export class ProductAlertsComponent implements OnInit {
   @Input() product!: Product;
-  constructor(){}
-ngOnInit(){
-
+  constructor() {}
+  ngOnInit() {}
 }
-
-} 
